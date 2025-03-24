2273224830550a437dc2e1e5ce6a3eab8ed94517
markcirineo <markcirineo22@gmail.com>
2025-03-23 23:58:12 -0400

    Create server

---
+++ packages/backend/src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+import config from "@/config";
+import logger from "@/logger";
+
+const app = express();
+const PORT = config.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(
+	morgan(
+		`[:date[iso]] - :method :url :status :response-time ms - :res[content-length] ":user-agent"`
+	)
+);
+
+app.listen(PORT, () => {
+	logger.info(`Server is running on port ${PORT}`);
+});
