1168dd8b0d5d29c113e1a36ebe8de13fed01a124
markcirineo <markcirineo22@gmail.com>
2025-03-23 23:23:42 -0400

    Add light-switch

--- packages/frontend/src/routes/+layout.svelte
+++ packages/frontend/src/routes/+layout.svelte
@@ -1,6 +1,23 @@
 <script lang="ts">
-	import '../app.css';
+	import "../app.css";
+	import { ModeWatcher } from "mode-watcher";
+	import LightSwitch from "$lib/components/ui/light-switch/light-switch.svelte";
+
 	let { children } = $props();
 </script>
 
+<ModeWatcher />
+
+<nav class="nav"><LightSwitch /></nav>
+
 {@render children()}
+
+<style>
+	.nav {
+		width: 100%;
+		padding: 1rem;
+		display: flex;
+		justify-content: end;
+		align-items: center;
+	}
+</style>
