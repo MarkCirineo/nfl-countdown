46e7ee0253cce651eddd9c9d3cabf0e117844764
markcirineo <markcirineo22@gmail.com>
2025-03-24 01:55:27 -0400

    Create auth routes

--- packages/backend/src/routes/index.ts
+++ packages/backend/src/routes/index.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { HttpError } from "@utils/error";
+import auth from "./auth";
 
 const router = Router();
+
+router.use("/auth", auth);
 
 router.get("*", (req, res, next) => {
 	next(new HttpError(`Route ${req.method}:${req.path} not found`, 404));
