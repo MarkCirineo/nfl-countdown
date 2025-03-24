6aa47a944e982e7c4f28e238d0673df34bd9f05f
markcirineo <markcirineo22@gmail.com>
2025-03-24 00:05:25 -0400

    Set up default routes

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -4,6 +4,7 @@ import morgan from "morgan";
 
 import config from "@/config";
 import logger from "@/logger";
+import router from "@/routes";
 
 const app = express();
 const PORT = config.PORT || 3000;
@@ -16,6 +17,8 @@ app.use(
 		`[:date[iso]] - :method :url :status :response-time ms - :res[content-length] ":user-agent"`
 	)
 );
+
+app.use(router);
 
 app.listen(PORT, () => {
 	logger.info(`Server is running on port ${PORT}`);
