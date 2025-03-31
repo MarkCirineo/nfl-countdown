64f509350b42c3d49ed5b983e9b7d7b8ff7287ef
markcirineo <markcirineo22@gmail.com>
2025-03-30 23:52:54 -0400

    Create /countdown/list route

--- packages/backend/src/routes/countdown/index.ts
+++ packages/backend/src/routes/countdown/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import create from "./create";
+import list from "./list";
 
 const router = Router();
 
 router.use("/create", create);
+router.use("/list", list);
 
 export default router;
