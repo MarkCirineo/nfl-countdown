5f00473883103693450cb08c835dc5e6ee45fdbb
markcirineo <markcirineo22@gmail.com>
2025-04-01 01:12:30 -0400

    Use countdown store

--- packages/frontend/src/lib/components/countdown/countdowns.svelte
+++ packages/frontend/src/lib/components/countdown/countdowns.svelte
@@ -1,38 +1,16 @@
 <script lang="ts">
 	import { onMount } from "svelte";
 
-	import type { Countdown as CountdownType, CountdownData } from "$lib/types/countdown";
 	import Countdown from "./countdown.svelte";
-	import { calculateTimeLeft } from "$lib/utils";
-
-	const fetchCountdowns = async (): Promise<CountdownType[]> => {
-		const response = await fetch("http://localhost:3000/api/countdown/list", {
-			method: "GET",
-			credentials: "include",
-			headers: {
-				"Content-Type": "application/json"
-			}
-		});
-
-		if (!response.ok) {
-			throw new Error("Failed to fetch countdowns");
-		}
-
-		return await response.json();
-	};
-
-	let countdownData: CountdownData[] = [];
+	import { countdownState, fetchCountdowns } from "$lib/stores/countdown.svelte";
 
 	onMount(async () => {
-		countdownData = (await fetchCountdowns()).map((countdown) => ({
-			...countdown,
-			timeLeft: calculateTimeLeft(countdown.date)
-		}));
+		await fetchCountdowns();
 	});
 </script>
 
 <ul class="space-y-4">
-	{#each countdownData as countdown}
+	{#each countdownState.countdownData as countdown}
 		<Countdown {countdown} />
 	{/each}
 </ul>
