4c8d38b72b6d2f8ec984690c2d5dd58515f0bd63
markcirineo <markcirineo22@gmail.com>
2025-04-02 00:15:54 -0400

    First pass at countdown page

---
+++ packages/frontend/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Countdowns from "$lib/components/countdown/countdowns.svelte";
+</script>
+
+<div class="overflow flex h-screen w-full flex-col items-center justify-start">
+	<div class="w-full max-w-3xl overflow-y-auto py-12">
+		<Countdowns />
+	</div>
+</div>
