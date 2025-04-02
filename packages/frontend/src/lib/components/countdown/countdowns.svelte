466cd121b8fb4309a27e1fa0bffe754aef76433a
markcirineo <markcirineo22@gmail.com>
2025-04-02 00:15:03 -0400

    Create admin-countdown and countdown components

--- packages/frontend/src/lib/components/countdown/countdowns.svelte
+++ packages/frontend/src/lib/components/countdown/countdowns.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import { onMount } from "svelte";
 
-	import Countdown from "./countdown.svelte";
 	import { countdownState, getCountdowns } from "$lib/stores/countdown.svelte";
+	import { authState } from "$lib/stores/auth.svelte";
+	import Countdown from "$lib/components/countdown/countdown.svelte";
+	import AdminCountdown from "./admin-countdown.svelte";
 
 	onMount(async () => {
 		await getCountdowns();
@@ -11,6 +13,10 @@
 
 <ul class="space-y-4">
 	{#each countdownState.countdownData as countdown}
-		<Countdown {countdown} />
+		{#if authState.user.role === "admin"}
+			<AdminCountdown {countdown} />
+		{:else}
+			<Countdown {countdown} />
+		{/if}
 	{/each}
 </ul>
