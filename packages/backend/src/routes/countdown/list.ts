7d85cdd15609e351befec328a769f7c344732ab4
markcirineo <markcirineo22@gmail.com>
2025-04-02 00:11:57 -0400

    Return simplified data for regular users

--- packages/backend/src/routes/countdown/list.ts
+++ packages/backend/src/routes/countdown/list.ts
@@ -1,12 +1,24 @@
 import { Router } from "express";
 import db from "@db";
 import { HttpError } from "@utils/error";
+import { Countdown } from "@db/tables/Countdown";
 
 const router = Router();
 
 router.get("/", async (req, res, next) => {
 	try {
-		const countdowns = await db.selectFrom("countdowns").selectAll().execute();
+		let countdowns:
+			| Countdown[]
+			| { id: number; title: string; description: string; date: Date }[] = [];
+
+		if (req.session.user?.role === "admin") {
+			countdowns = await db.selectFrom("countdowns").selectAll().execute();
+		} else {
+			countdowns = await db
+				.selectFrom("countdowns")
+				.select(["id", "title", "description", "date"])
+				.execute();
+		}
 
 		if (countdowns.length === 0) {
 			return res.status(404).send({ message: "No countdowns found" });
