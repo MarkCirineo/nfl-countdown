ac8691dd31b221a3bbc23b2cb829cbe70a04d3b8
markcirineo <markcirineo22@gmail.com>
2025-04-07 00:37:10 -0400

    Create editing-countdown component

--- packages/frontend/src/lib/components/countdown/countdown.svelte
+++ packages/frontend/src/lib/components/countdown/countdown.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
 	import { onDestroy } from "svelte";
+
 	import Button from "$lib/components/ui/button/button.svelte";
+
 	import type { CountdownData } from "$lib/types/countdown";
 	import { calculateTimeLeft, cn } from "$lib/utils";
 	import { getCountdowns } from "$lib/stores/countdown.svelte";
+	import EditingCountdown from "./editing-countdown.svelte";
 
 	let { countdown, isAdmin }: { countdown: CountdownData; isAdmin: boolean } = $props();
 
-	const editCountdown = (id: number) => {
-		// Placeholder for editing a countdown
-	};
+	let editing = $state(false);
 
 	const deleteCountdown = async (id: number) => {
 		const response = await fetch("http://localhost:3000/api/countdown/delete", {
@@ -42,50 +43,64 @@
 	});
 </script>
 
-<li
-	class={cn(
-		"flex min-h-[82px] items-center justify-between rounded border p-4 shadow",
-		countdown.timeLeft.isPast &&
-			"bg-gray-100 text-gray-500 opacity-85 dark:bg-gray-900 dark:text-gray-600"
-	)}
->
-	<div class="flex-1">
-		<h2 class="text-lg font-bold">{countdown.title}</h2>
-		<p class={cn("text-sm text-gray-400", countdown.timeLeft.isPast && "text-gray-500")}>
-			{countdown.description}
-		</p>
-		{#if isAdmin}
+{#if editing}
+	<EditingCountdown bind:editing {countdown} />
+{:else}
+	<li
+		class={cn(
+			"flex min-h-[82px] items-center justify-between rounded border p-4 shadow",
+			countdown.timeLeft.isPast &&
+				"bg-gray-100 text-gray-500 opacity-85 dark:bg-gray-900 dark:text-gray-600"
+		)}
+	>
+		<div class="flex-1">
+			<h2 class="text-lg font-bold">{countdown.title}</h2>
 			<p class={cn("text-sm text-gray-400", countdown.timeLeft.isPast && "text-gray-500")}>
-				Date: {countdown.date}
+				{countdown.description}
 			</p>
-			<p class={cn("text-sm text-gray-400", countdown.timeLeft.isPast && "text-gray-500")}>
-				Created By: {countdown.created_by}
-			</p>
-		{/if}
-	</div>
-	<div class="flex items-center space-x-4">
-		{#if countdown.timeLeft}
-			<div class="text-center">
-				{#if countdown.timeLeft.isPast}
-					<p class="text-lg font-bold">
-						{countdown.timeLeft.days}d {countdown.timeLeft.hours}h {countdown.timeLeft
-							.minutes}m {countdown.timeLeft.seconds}s ago
-					</p>
-				{:else}
-					<p class="text-lg font-bold text-blue-500">
-						{countdown.timeLeft.days}d {countdown.timeLeft.hours}h {countdown.timeLeft
-							.minutes}m {countdown.timeLeft.seconds}s
-					</p>
-				{/if}
-			</div>
-		{/if}
-		{#if isAdmin}
-			<div class="flex space-x-2">
-				<Button onclick={() => editCountdown(countdown.id)}>Edit</Button>
-				<Button onclick={() => deleteCountdown(countdown.id)} class="bg-red-500"
-					>Delete</Button
+			{#if isAdmin}
+				<p
+					class={cn(
+						"text-sm text-gray-400",
+						countdown.timeLeft.isPast && "text-gray-500"
+					)}
 				>
-			</div>
-		{/if}
-	</div>
-</li>
+					Date: {countdown.date}
+				</p>
+				<p
+					class={cn(
+						"text-sm text-gray-400",
+						countdown.timeLeft.isPast && "text-gray-500"
+					)}
+				>
+					Created By: {countdown.created_by}
+				</p>
+			{/if}
+		</div>
+		<div class="flex items-center space-x-4">
+			{#if countdown.timeLeft}
+				<div class="text-center">
+					{#if countdown.timeLeft.isPast}
+						<p class="text-lg font-bold">
+							{countdown.timeLeft.days}d {countdown.timeLeft.hours}h {countdown
+								.timeLeft.minutes}m {countdown.timeLeft.seconds}s ago
+						</p>
+					{:else}
+						<p class="text-lg font-bold text-blue-500">
+							{countdown.timeLeft.days}d {countdown.timeLeft.hours}h {countdown
+								.timeLeft.minutes}m {countdown.timeLeft.seconds}s
+						</p>
+					{/if}
+				</div>
+			{/if}
+			{#if isAdmin}
+				<div class="flex space-x-2">
+					<Button onclick={() => (editing = true)}>Edit</Button>
+					<Button onclick={() => deleteCountdown(countdown.id)} class="bg-red-500"
+						>Delete</Button
+					>
+				</div>
+			{/if}
+		</div>
+	</li>
+{/if}
---
+++ packages/frontend/src/lib/components/countdown/editing-countdown.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+	import { parseDate, type DateValue } from "@internationalized/date";
+
+	import Textarea from "$lib/components/ui/textarea/textarea.svelte";
+	import DatePicker from "$lib/components/ui/date-picker/date-picker.svelte";
+	import Input from "$lib/components/ui/input/input.svelte";
+	import TimePicker from "$lib/components/ui/time-picker/time-picker.svelte";
+	import Button from "$lib/components/ui/button/button.svelte";
+
+	import { cn } from "$lib/utils";
+	import type { CountdownData } from "$lib/types/countdown";
+	import { getCountdowns } from "$lib/stores/countdown.svelte";
+
+	let { countdown, editing = $bindable() }: { countdown: CountdownData; editing: boolean } =
+		$props();
+
+	let date: DateValue | undefined = $state(parseDate(countdown.date.split("T")[0]));
+	let time = $state(countdown.date.split("T")[1].slice(0, 5));
+	let title = $state(countdown.title);
+	let description = $state(countdown.description);
+
+	const cancelEditingCountdown = () => {
+		editing = false;
+	};
+
+	const editCountdown = async () => {
+		if (!date || !time || !title) {
+			// TODO: toast?
+			return;
+		}
+
+		const dateAndTime = `${date.toString()}T${time}:00`;
+
+		// Date validation
+		const parsedDate = Date.parse(dateAndTime);
+		if (isNaN(parsedDate)) {
+			// TODO: toast?
+			return;
+		}
+
+		const response = await fetch("http://localhost:3000/api/countdown/update", {
+			method: "PUT",
+			credentials: "include",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({
+				id: countdown.id,
+				date: dateAndTime,
+				title,
+				description
+			})
+		});
+
+		if (response.ok) {
+			cancelEditingCountdown();
+			getCountdowns();
+		} else {
+			// TODO: handle error
+		}
+	};
+</script>
+
+<div class="mb-4 rounded border p-4 shadow">
+	<h2 class="text-lg font-bold">New Countdown</h2>
+	<div class="mt-2 space-y-2">
+		<Input
+			type="text"
+			placeholder="Title"
+			class="w-full rounded border px-2 py-1"
+			bind:value={title}
+		/>
+		<Textarea
+			placeholder="Description"
+			class="w-full rounded border px-2 py-1"
+			bind:value={description}
+		/>
+		<div class="flex space-x-2">
+			<DatePicker
+				passedClass={cn(
+					"flex-1 rounded border px-2 py-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
+				)}
+				bind:value={date}
+			/>
+			<TimePicker
+				passedClass={cn(
+					"flex-1 rounded border px-2 py-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
+				)}
+				bind:selectedTime={time}
+			/>
+		</div>
+	</div>
+	<div class="mt-4 flex space-x-2">
+		<Button onclick={cancelEditingCountdown} class="bg-gray-400">Cancel</Button>
+		<Button onclick={editCountdown} class="bg-green-500">Save</Button>
+	</div>
+</div>
