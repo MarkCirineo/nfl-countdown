086d857c8cd02c45b949c6d2c05fbf768042b52e
markcirineo <markcirineo22@gmail.com>
2025-06-17 21:15:59 -0400

    Fix env in prod

--- packages/frontend/src/routes/+layout.svelte
+++ packages/frontend/src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 	import LightSwitch from "$lib/components/ui/light-switch/light-switch.svelte";
 	import LogOut from "$lib/components/ui/log-out/log-out.svelte";
 	import { getUser, authState } from "$lib/stores/auth.svelte";
-	const { VITE_ADSENSE_ID } = import.meta.env;
+	import { env } from "$env/dynamic/public";
 
 	let { children } = $props();
 
@@ -17,7 +17,7 @@
 <svelte:head>
 	<script
 		async
-		src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client={VITE_ADSENSE_ID}"
+		src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client={env.PUBLIC_ADSENSE_ID}"
 		crossorigin="anonymous"
 	></script>
 </svelte:head>
